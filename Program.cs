122b4d502f337f64dce4e19a5e46661922ed218a
Hilmi Egemen Ciritoglu <eciritoglu94@gmail.com>
2015-07-25 18:22:05 +0300

    v 1.00

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace CurrencyBot
     {
         static void Main(string[] args)
         {
+            #region second alternative
             /*
             Uri url = new Uri("http://www.tcmb.gov.tr/kurlar/today.xml");
 
@@ -46,34 +47,68 @@ namespace CurrencyBot
             }
 
              */
-            //First buy Second sell
-            String[,] moneys;  
-
-            String PostUrl = "http://www.tcmb.gov.tr/kurlar/today.xml";
-            WebResponse webResponse = WebRequest.Create(PostUrl).GetResponse();
-            StreamReader sr = new StreamReader(webResponse.GetResponseStream(), Encoding.UTF8);
-            String Result = sr.ReadToEnd().Trim();
-
-            XmlDocument xdoc = new XmlDocument(); 
-            xdoc.LoadXml(Result);
-            
-            XmlNodeList NodeList =
-                xdoc.SelectNodes("Tarih_Date/Currency");
-
-            moneys = new String[NodeList.Count, 3];
-            for (int counter=0; counter<NodeList.Count-1;counter++)
-            {
-                moneys[counter,0] = NodeList[counter].SelectSingleNode("CurrencyName").InnerText;
-                moneys[counter,1] = NodeList[counter].SelectSingleNode("ForexBuying").InnerText;
-                moneys[counter,2] = NodeList[counter].SelectSingleNode("ForexSelling").InnerText;
-                
-                Console.WriteLine("Currency: "+moneys[counter, 0]);
-                Console.WriteLine("Buying rates : " + moneys[counter, 1]);
-                Console.WriteLine("Selling rates : " + moneys[counter, 2]+"\n");       
-
-            }
+            //First buy Second sell 
+            #endregion
+           
+            String Text = getDataFromWebPage("http://www.tcmb.gov.tr/kurlar/today.xml");
+            XmlDocument xdoc = createXML(Text);
+            String[,]currencyRates=getCurrenyRates(xdoc);
+            printCurrency(currencyRates);
 
 
         }
+
+        private static String[,] getCurrenyRates(XmlDocument xdoc) {
+            String[,] currencyInformation;
+            XmlNodeList NodeList =
+                xdoc.SelectNodes("Tarih_Date/Currency");
+            // ParentNode 
+            // 0 date dd.mm.yyyy
+            // 1 date mm/dd.yyyy
+            // 2 bulletin no
+
+            currencyInformation = new String[NodeList.Count, 5];
+            for (int counter = 0; counter < NodeList.Count - 1; counter++)
+            {
+                currencyInformation[counter, 0] = NodeList[counter].SelectSingleNode("Unit").InnerText;
+                currencyInformation[counter, 1] = NodeList[counter].SelectSingleNode("CurrencyName").InnerText;
+                currencyInformation[counter, 2] = NodeList[counter].SelectSingleNode("ForexBuying").InnerText;
+                currencyInformation[counter, 3] = NodeList[counter].SelectSingleNode("ForexSelling").InnerText;
+                currencyInformation[counter, 4] = "Date : "+NodeList[counter].ParentNode.Attributes["Tarih"].Value + " Bulletin Number: " +
+                                                                        NodeList[counter].ParentNode.Attributes["Bulten_No"].Value;               
+            }
+            return currencyInformation;
+                   
+        }
+
+        private static void printCurrency(String[,] currencyInformation) {
+            Console.WriteLine("==============================================================");
+            Console.WriteLine("== Latest Update "+currencyInformation[0,4]+"==");
+            Console.WriteLine("==============================================================\n");
+
+
+            for (int counter = 0; counter < currencyInformation.GetLength(0)-1; counter++) {
+                Console.WriteLine(" == " + currencyInformation[counter, 0] + "  "+ currencyInformation[counter, 1] + " == ");
+                Console.WriteLine("Buying rates : " + currencyInformation[counter, 2]+" TL ");
+                Console.WriteLine("Selling rates : " + currencyInformation[counter, 3] + " TL " + "\n");
+            }
+        }
+       
+
+        private static XmlDocument createXML(String text) {
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.LoadXml(text);
+            return xdoc;            
+        }
+
+        private static string getDataFromWebPage(String url){
+            String PostUrl = url;
+            WebResponse webResponse = WebRequest.Create(PostUrl).GetResponse();
+            StreamReader sr = new StreamReader(webResponse.GetResponseStream(), Encoding.UTF8);
+            String Result = sr.ReadToEnd().Trim();
+            return Result;
+        }
+
+       
     }
 }
